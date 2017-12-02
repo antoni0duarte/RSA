51ae892e5d5fd6e9616842a3df136c32cc6c4ab9
Antonio Duarte <30534210+antoni0duarte@users.noreply.github.com>
2017-12-02 17:55:58 -0200

    Update cifra_de_césar.py

--- cifra_de_césar.py
+++ cifra_de_césar.py
@@ -1,3 +1,12 @@
+'''
+Autor: Antonio Lucas de Oliveira Duarte Bezerra
+Projeto implementado: Cifra de César    
+Descrição do algoritmo: Um deslocamento de 15 caracteres na tabela ASCII.
+Descrição de como compilar/executar o programa: 
+- Abra o arquivo na ide
+- Para criptografar utilize: encripta('Mensagem')
+- Para descriptografar utilize: decripta('Código criptografado')
+'''
 def encripta(msg):
     s = ''
     for c in msg: s = s + chr(ord(c) + 15)
